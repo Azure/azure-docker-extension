7d52065a8855aef3ececb46ecb8244e4548f4cec
Kevin Ickes <kickes@users.noreply.github.com>
2015-02-27 08:53:10 -0800

    Update enable.sh
    With the release of 1.1.0 Fig was renamed to compose which also resulted in them changing the repository name. While GitHub continues to redirect web traffic in instances like this, it is probably best to update it to help reduce confusion.

--- scripts/enable.sh
+++ scripts/enable.sh
@@ -77,7 +77,7 @@ else
     compose_up="false"
 fi
 
-curl -L https://github.com/docker/fig/releases/download/1.1.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.1.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 if [ "$compose_up" != "false" ]; then
