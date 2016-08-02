9f076eb5b5b5b4abf7e2064a9d37d0050d7e9739
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-08-02 14:47:21 -0700

    pkg/vmextension: remove SeqNo field from HandlerEnv type

--- pkg/vmextension/handlerenv_test.go
+++ pkg/vmextension/handlerenv_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_ParseHandlerEnv(t *testing.T) {
-	json := `[{  "name": "MSOpenTech.Extensions.DockerExtension", "seqNo": "0", "version": 1.0,  "handlerEnvironment": {    "logFolder": "/var/log/azure/MSOpenTech.Extensions.DockerExtension/0.6.0.0",    "configFolder": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/config",    "statusFolder": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/status",    "heartbeatFile": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/heartbeat.log"}}]`
+	json := `[{  "name": "MSOpenTech.Extensions.DockerExtension", "version": 1.0,  "handlerEnvironment": {    "logFolder": "/var/log/azure/MSOpenTech.Extensions.DockerExtension/0.6.0.0",    "configFolder": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/config",    "statusFolder": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/status",    "heartbeatFile": "/var/lib/waagent/MSOpenTech.Extensions.DockerExtension-0.6.0.0/heartbeat.log"}}]`
 	c, err := ParseHandlerEnv([]byte(json))
 	if err != nil {
 		t.Fatal(err)
