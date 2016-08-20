fcf85c70a6eccafa20371db55c00da5d0686e1e0
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-08-20 05:27:21 +0000

    systemd: update ExecStart with dockerd and -H=unix://
    - get.docker.com script has removed support for -H=fd:// on
      centos, therefore updating it to use -H=unix:// by default on
      centos.
    - also switching to dockerd over 'docker daemon' in ExecStart of
      systemd unit files
    
    Fixes #104.

--- pkg/dockeropts/upstart_test.go
+++ pkg/dockeropts/upstart_test.go
@@ -19,7 +19,7 @@ func TestUpstartEditor_UbuntuDefault(t *testing.T) {
 	in := `# Docker Upstart and SysVinit configuration file
 
 # Customize location of Docker binary (especially for development testing).
-#DOCKER="/usr/local/bin/docker"
+#DOCKER="/usr/local/bin/dockerd"
 
 # Use DOCKER_OPTS to modify the daemon startup options.
 #DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"
@@ -33,7 +33,7 @@ func TestUpstartEditor_UbuntuDefault(t *testing.T) {
 	expected := `# Docker Upstart and SysVinit configuration file
 
 # Customize location of Docker binary (especially for development testing).
-#DOCKER="/usr/local/bin/docker"
+#DOCKER="/usr/local/bin/dockerd"
 
 # Use DOCKER_OPTS to modify the daemon startup options.
 DOCKER_OPTS="-d --tlsverify"
